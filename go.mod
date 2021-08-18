89d2d8c9832c4e80265f9864eb73c264b3472a41
juchilian <juchilian1227@gmail.com>
2021-08-18 21:40:23 +0900

    [update] redisに保存

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module example.com/m/v2
 go 1.16
 
 require (
+	github.com/garyburd/redigo v1.6.2
+	github.com/pkg/errors v0.9.1
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
 	golang.org/x/sys v0.0.0-20210816074244-15123e1e1f71 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -7,6 +7,8 @@ import (
 	"net"
 
 	"example.com/m/v2/greet/greetpb"
+	"example.com/m/v2/repository"
+	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 )
 
@@ -15,6 +17,29 @@ type server struct {
 
 func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
 	fmt.Printf("Greet function was invoked with %v\n", req)
+	err := saveToRedis(req)
+	if err != nil {
+		log.Fatalf("Failed to Save to Redis: %v", err)
+	}
+	res, err := makeResponse(req)
+	if err != nil {
+		log.Fatalf("Failed to make Response: %v", err)
+	}
+	return res, nil
+}
+
+func saveToRedis(req *greetpb.GreetRequest) error {
+	r := repository.NewRedis()
+	defer r.CloseRedis()
+
+	// Set to Redis
+	if err := r.Set("FirstName", []byte(req.GetGreeting().GetFirstName())); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func makeResponse(req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
 	firstName := req.GetGreeting().GetFirstName()
 	result := "Hello " + firstName
 	res := greetpb.GreetResponse{
@@ -33,8 +58,12 @@ func main() {
 
 	s := grpc.NewServer()
 	greetpb.RegisterGreetServiceServer(s, &server{})
+	// Save to Redis
 
+	// Send back
+	fmt.Printf("Before Serve")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+	fmt.Printf("After Serve")
 }
